da22d087bdda22fdf90ed4650bae3c14a8b2c1c4
goldenowl281 <119384110+goldenowl281@users.noreply.github.com>
2023-03-08 04:24:00 +0630

    Update database.py

--- database.py
+++ database.py
@@ -1,163 +1,99 @@
-import database
-import client
-import copyclient
-import server
+import pymongo
+from pymongo import MongoClient
 import socket
 
 
-class Data:
+class Mongodatabase:
+    try:
+        clients = MongoClient('localhost', 27017)
+        db = clients['Delivery']
+
+        collection = db['shop-collection']
+        collection_1 = db['User_collection']
+
+        print("connection success")
+    except Exception as error:
+        print(error)
+
     def __init__(self):
-        pass
+        data = {"_id": 13, "name": "sithuaung", "phoneNumber": 95911111111, "email": "sithu@gmail.com",
+                "password": "sithuaung", "history": []}
+        try:
+            self.collection_1.insert_one(data)
+            print("insert successful")
+        except Exception as error:
+            print(error)
 
-    def createAcc(self):
-        user = "Enter user_name--:#Enter your phoneNumber--:#Enter your email--:#Enter your password--:#Enter confirm " \
-               "password--:"
-        return user
+        # def option(self):
+        #     chooseOption = "PRESS 1: TO Create Account--:#nPRESS 2: TO Sign_in Account--:#PRESS 3: To Show Menu--:"
+        #     return chooseOption
 
-    def storeData(self, sData):
-        obj2 = database.Mongodatabase()
-        id_order = obj2.clients["Delivery"]["User_collection"]
-        id_count = id_order.count_documents({}) + 1
-        print(id_count)
-        splitDT = sData.split("#")
-        print(splitDT)
-        splitDT.insert(0, id_count)
-        print(splitDT)
-
-        if splitDT[5] == splitDT[4]:
-            list1 = ["_id", "name", "phoneNumber", "email", "password"]
-            splitDT.remove(splitDT[5])
-
-            rec_phone_number = self.checkPhNumber(splitDT[2])
-            splitDT[2] = rec_phone_number
-
-            if rec_phone_number != False:
-                # splitDT.insert(2, rec_phone_number)
-                print(splitDT)
-                zipData = zip(list1, splitDT)
-                print("your password correct", zipData)
-                zp = dict(zipData)
-                obj2.collection_1.insert_one(zp)
-                print("insert successful")
-                return True
-
-            else:
-                return False
-
-
-        else:
-            return False
-
-    def checkPhNumber(self, phNumber):
-        obj2 = database.Mongodatabase()
-        print(type(phNumber))
-        intPhNumber = int(phNumber)
-        print(type(intPhNumber))
-        if len(phNumber) >= 8:
-            add_data = "959"
-            fullNumber = add_data + phNumber
-            print(fullNumber)
-            user_collection = obj2.collection_1
-            print("hello")
-            # phone_numbers = user_collection.find({}, {"phoneNumber": 1}, {"_id": 0})
-            phone_numbers = user_collection.find({}, {"phoneNumber": 1}, skip=0)
-            for user in phone_numbers:
-                if user["phoneNumber"] == fullNumber:
-                    print("phoneNumber already exit\n\n\n")
-                    return False
-            return fullNumber
-
-        else:
-            print("not correct")
-            return False
-
-    def login_information(self):
-        user = "Enter your phone number--#/Enter your password--:"
-        return user
-
-    def login_account(self, rec_InputData):
-        obj2 = database.Mongodatabase()
-        splitData = rec_InputData.split("#")
-        print(splitData)
-        user_collection = obj2.collection_1
-        print("helloworld")
-        phoneNumber = splitData[0]
-        password1 = splitData[1]
-        add_data = "959"
-        fullNumber = add_data + phoneNumber
-        phoneNumber1 = fullNumber
-
-        db_ph_numbers = user_collection.find_one({"phoneNumber": phoneNumber1})
-        if db_ph_numbers:
-            print("\nphone number found")
-            stored_password = db_ph_numbers["password"]
-            print(stored_password)
-            if stored_password == password1:
-                print("\n\nright password")
-                return True
-            else:
-                print("\n\nwrong password")
-                return False
-        else:
-            print("\n\nphone number not found")
-            return False
-
-    def menuOption(self):
-        menu_option = "\n\n           I have this item::\nPRESS 1: soft drink--:#Press 2: Sea Foods--:#Press 3: BBQ--#PRESS 4: Cakes--:# PRESS 5: Foods--:# PRESS 6: show all menu--:"
-
-        return menu_option
-
-    def get_shop_name(self):
-        obj2 = database.Mongodatabase()
-        collections = obj2.collection
-        # Find all shops that have 'soft drink' menu
-        shops_with_soft_drinks = collections.find({"soft drink": {"$exists": True}})
-
-        # Create a dictionary to store shop names with 'soft drink' menu
-        shop_dict = {}
-
-        # Loop through all shops with 'soft drink' menu and add shop names and soft drinks to the dictionary
-        for shop in shops_with_soft_drinks:
-            shop_name = shop["Shop Name"]
-            soft_drinks = ", ".join([f"{drink['name']} ({drink['price']}ks)" for drink in shop["soft drink"]])
-            shop_dict[shop_name] = soft_drinks
-
-        # Return a formatted string with shop names and 'soft drink' menu
-        output_str = ""
-        for shop_name, soft_drinks in shop_dict.items():
-            output_str += f"\n{shop_name}: {soft_drinks}\n"
-        return output_str
-
-    def search_result(self, shops, menu_option1):
-        output = ""
-        for shop in shops:
-            items = shop.split(': ')
-            shop_name = items[0]
-            item_list = items[1].split(', ')
-            i = 1
-            for item in item_list:
-                name, price = item.split(' (')
-                price = price[:-3]  # to rejuce last three word
-                if name == menu_option1:  # check if menu_option1 matches the item name
-                    output += f'{shop_name}\n{i}-  {name}: {price} ks \n'
-                    i += 1
-        return output
-
-    def search_result1(self, shops):
-        for shop in shops:
-            items = shop.split(': ')
-            shop_name = items[0]
-            item_list = items[1].split(', ')
-            print('\nShop Name:', shop_name)
-            i = 1
-            for item in item_list:
-                name, price = item.split(' (')
-                price = price[:-3]  # to rejuce last three word
-                print(f'{i}-  {name}: {price} ks')
-                i += 1
-
-    def buy_option(self):
-        buyOption = "\nPRESS 1: To cert--:#PRESS 2: To Cancel--:#PRESS 3: To confirm order--:#PRESS 4 : To choose more item--:#PRESS 5: To Exit--:"
-        return buyOption
+        # Press 2: Sea Foods--:# Press 3: BBQ--# PRESS 4: " \
+        # "Cakes--:# PRESS 5: Foods--:# PRESS 6: show all menu--:"
+        data1 = [
+            {
+                "_id": 1, "Shop Name": "PanEi",
+                "soft drink": [
+                    {"name": "coke", "price": 2000},
+                    {"name": "sprite", "price": 2000},
+                    {"name": "lemonade", "price": 2000},
+                    {"name": "Iced Tea", "price": 2000}
+                ],
+                "foods": [
+                    {"name": "shan noodle", "price": 3000},
+                    {"name": "coconut chicken noodle", "price": 3000},
+                    {"name": "chicken curry", "price": 5000}
+                ]
+            },
+            {
+                "_id": 2, "Shop Name": "TeaPro",
+                "soft drink": [
+                    {"name": "Apple Juice", "price": 3000},
+                    {"name": "GrapeFruit Juice", "price": 3000},
+                    {"name": "Hot Chocolate", "price": 4000}
+                ],
+                "foods": [
+                    {"name": "NanGyi Toke", "price": 3000},
+                    {"name": "Mohinga", "price": 2000},
+                    {"name": "chicken liver & Gizzard", "price": 5000}
+                ]
+            },
+            {
+                "_id": 3, "Shop Name": "OceanWave",
+                "Sea Foods": [
+                    {"name": "Fish and Chips", "price": 7000},
+                    {"name": "Crab Meat", "price": 10000},
+                    {"name": "Salmon", "price": 10000}
+                ],
+                "BBQ": [
+                    {"name": "BBQ special", "price": 20000},
+                    {"name": "Spicy Beef Bomb", "price": 20000},
+                    {"name": "Chicken Wings", "price": 10000}
+                ]
+            },
+            {
+                "_id": 4, "Shop Name": "FudoBakery",
+                "Cakes": [
+                    {"name": "Butter Cake", "price": 2000},
+                    {"name": "Chick-pea Cake", "price": 5000},
+                    {"name": "Cheese-Topping Cake", "price": 5000}
+                ],
+                "soft drink": [
+                    {"name": "Strawberry Delight", "price": 3000},
+                    {"name": "Ice Thai Tea", "price": 3000},
+                    {"name": "Faluda", "price": 4000},
+                    {"name": "Burbble Milktea", "price": 3000},
+                    {"name": "Blueberry Delight", "price": 3000}
+                ]
+            }
+        ]
+        # try:
+        #     self.collection.drop()
+        #     self.collection.insert_many(data1)
+        #     print("insert successful")
+        # except Exception as error:
+        #     print(error)
 
 
+if __name__ == "__main__":
+    obj = Mongodatabase()

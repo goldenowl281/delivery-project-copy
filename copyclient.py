9a892148312d009afb4ec7c31ee4e24cf907c75a
goldenowl281 <119384110+goldenowl281@users.noreply.github.com>
2023-03-07 08:12:22 +0630

    Update copyclient.py

--- copyclient.py
+++ copyclient.py
@@ -6,13 +6,14 @@ import allData
 class TCPclient:
     def __init__(self):
         self.server_ip = 'localhost'
-        self.server_port = 8084
+        self.server_port = 8082
 
     def main(self):
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             client.connect((self.server_ip, self.server_port))
             print("connection success\n")
+            flag = True
 
             while True:
                 print("\n\nPRESS 1: TO Create Account")
@@ -53,44 +54,67 @@ class TCPclient:
                     print(recData)
 
                 elif option == "3":
-                    client.send(option.encode())
+                    while flag:
+                        client.send(option.encode())
 
-                    # Receive and display menu option from server
-                    rec_from_server = client.recv(4096)
-                    print(f'\n[&&]Receive from server :{rec_from_server.decode()}\n')
-                    rec_from_server = rec_from_server.decode()
-                    item = rec_from_server.split("#")
-                    # print(item)
-                    for menu in item:
-                        print(menu)
-                    # get user input
-                    menu_option = input("Enter your option: ")
-                    client.send(menu_option.encode())
-
-                    # Receive and display menu option from server
-                    rec_from_server1 = client.recv(4096)
-                    print(f'\n[&&]Receive from server :\n{rec_from_server1.decode()}\n')
-                    print(type(rec_from_server1))
-                    rec_from_server1 = rec_from_server1.decode()
-                    print(rec_from_server1)
-                    shops = rec_from_server1.split("\n\n")
-                    obj.search_result1(shops)
-                    menu_option1 = input("\nI have this item \nSelete what you want to take by name:--:")
-                    client.send(menu_option1.encode())
-                    found = client.recv(1024).decode("utf-8")
-
-                    if found != "None":
-                        print(found)
-                        option_buy = client.recv(1024).decode("utf-8")
-                        choices = option_buy.split("#")
-                        for menu in choices:
+                        # Receive and display menu option from server
+                        rec_from_server = client.recv(4096)
+                        print(f'\n[&&]Receive from server :{rec_from_server.decode()}\n')
+                        rec_from_server = rec_from_server.decode()
+                        item = rec_from_server.split("#")
+                        # print(item)
+                        for menu in item:
                             print(menu)
-                        option_Buy = input("Enter your option:  ")
-                        if option_Buy == "1":
-                            buy_total = input("Choose how many items do you  want--:")
-                            client.send(buy_total.encode())
-                    else:
-                        print("Item not found.")
+                        # get user input
+                        menu_option = input("Enter your option: ")
+                        client.send(menu_option.encode())
+
+                        # Receive and display menu option from server
+                        rec_from_server1 = client.recv(4096)
+                        print(f'\n[&&]Receive from server :\n{rec_from_server1.decode()}\n')
+                        print(type(rec_from_server1))
+                        rec_from_server1 = rec_from_server1.decode()
+                        print(rec_from_server1)
+                        shops = rec_from_server1.split("\n\n")
+                        obj.search_result1(shops)
+                        menu_option1 = input("\nI have this item \nSelete what you want to take by name:--:")
+                        client.send(menu_option1.encode())
+                        found = client.recv(1024).decode("utf-8")
+
+                        if found != "None":
+                            print("my order", found)
+                            option2_buy = client.recv(1024)
+                            option2_buy = option2_buy.decode("utf-8")
+                            print("splitlskdk", type(option2_buy))
+
+                            choices = option2_buy.split("#")
+                            while True:
+                                for menu in choices:
+                                    print(menu)
+                                option_Buy = input("Enter your option:  ")
+
+                                if option_Buy == "1":
+                                    buy_total = input("Choose how many items do you  want--:")
+                                    client.send(buy_total.encode())
+                                    price = client.recv(4096).decode("utf-8")
+                                    print("Your total amount", price)
+                                    # for menu in choices:
+                                    #     print(menu)
+                                    # option_Buy = input("Enter your option:  ")
+
+                                if option_Buy == "5":
+                                    client.send(option_Buy.encode())
+                                    flag = False
+
+                                if option_Buy == "4":
+                                    client.send(option_Buy.encode())
+                                    break
+
+                                    flag = False
+
+
+                        else:
+                            print("Item not found.")
 
 
 if __name__ == "__main__":
